b3840b5bd1821cdbdb966b33cfadf6aa67923ee7
Hector Ricardo Lopez Pepa <heturpepa@gmail.com>
2025-03-25 18:05:33 -0300

    degundo intento

--- rasa-widget.js
+++ rasa-widget.js
@@ -1,21 +1,19 @@
-window.onload = () => {
-    const script = document.createElement("script");
-    script.src = "https://cdn.jsdelivr.net/gh/botfront/rasa-webchat@master/lib/index.js";
+(function () {
+    var script = document.createElement('script');
+    script.src = 'https://cdn.jsdelivr.net/npm/rasa-webchat@1.0.1/lib/index.js';
     script.async = true;
-  
-    script.onload = () => {
-      WebChat.default({
-        initPayload: "/saludo",
-        customData: { language: "es" },
-        socketUrl: "https://cc44-181-1-53-69.ngrok-free.app", // Tu ngrok actual
-        socketPath: "/socket.io/",
-        title: "Asistente Virtual",
-        subtitle: "¿En qué puedo ayudarte?",
-        inputTextFieldHint: "Escribe tu consulta...",
+    script.onload = function () {
+      window.WebChat.default({
+        initPayload: '/saludo',
+        customData: { language: 'es' },
+        socketUrl: 'https://98f4-181-1-53-69.ngrok-free.app',
+        socketPath: '/socket.io/',
+        title: 'Asistente Virtual',
+        subtitle: '¿En qué puedo ayudarte?',
+        inputTextFieldHint: 'Escribe tu consulta...',
         showFullScreenButton: true,
       }, null);
     };
-  
-    document.body.appendChild(script);
-  };
+    document.head.appendChild(script);
+  })();
   
