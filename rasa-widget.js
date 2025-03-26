d334f3d59d9220ac43d72b336c52823d9e541506
Hector Ricardo Lopez Pepa <heturpepa@gmail.com>
2025-03-26 10:59:11 -0300

    archivo modificado js

--- rasa-widget.js
+++ rasa-widget.js
@@ -2,7 +2,7 @@ window.onload = () => {
     WebChat.default({
       initPayload: "/saludo",
       customData: { language: "es" },
-      socketUrl: "https://TU_SUBDOMINIO.ngrok-free.app",  // cambia por tu URL activa
+      socketUrl: "https://65a4-181-1-53-69.ngrok-free.app",  // cambia por tu URL activa
       socketPath: "/socket.io/",
       title: "Asistente Virtual",
       subtitle: "¿En qué puedo ayudarte?",
